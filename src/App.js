5d1897286ee0d97fb187feafe7ba049aeeb7b7fe
mselmi <mselmi@mariems-MacBook-Pro.local>
2020-11-21 14:03:18 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -4,25 +4,17 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
-    <div className="App">
-    <img className="logo"></img>
-      <form>
-  <div className="form-group">
-    <label for="exampleInputEmail1">Email address</label>
-    <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email"/>
-    <small id="emailHelp" className="form-text text-muted">We'll never share your email with anyone else.</small>
+  <>
+  <div style={{border:"solid 1px black", maxWidth:"100vw"}}>
+  <div className='titlered'>Name</div>
+  <br/>
+  <img src="./bordgena.jpeg"/>
+  <br/>
+  <img src="./lemon.jpg"/>
   </div>
-  <div className="form-group">
-    <label for="exampleInputPassword1">Password</label>
-    <input type="password" className="form-control" id="exampleInputPassword1" placeholder="Password"/>
-  </div>
-  <div className="form-check">
-    <input type="checkbox" className="form-check-input" id="exampleCheck1"/>
-    <label className="form-check-label" for="exampleCheck1">Check me out</label>
-  </div>
-  <button type="submit" className="btn btn-primary">Submit</button>
-</form>
-    </div>
+  <video width="320" height="240" controls/>
+  </>
+
   );
 }
 
